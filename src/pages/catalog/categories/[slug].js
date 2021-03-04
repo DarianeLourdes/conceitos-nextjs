e43f70c41eeb2a6ab8b254e4dcc9caec768d7655
darianelourdes <darianelsantana@gmail.com>
2021-03-03 21:51:28 -0300

    dynamic static pages

---
+++ src/pages/catalog/categories/[slug].js
@@ -0,0 +1,53 @@
+import { useRouter } from 'next/router';
+
+export default function Category({ products }) {
+  const router = useRouter();
+
+  return (
+    <div>
+        <h1>{router.query.slug}</h1>
+
+        <ul>
+        {products.map(product => {
+          return (
+            <li key={product.id}>
+              {product.title}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+   
+}
+
+
+export const getStaticPaths = async () => {
+  const response = await fetch("http://localhost:3333/categories")
+  const categories = await response.json();
+
+  const paths = categories.map(category => {
+    return {
+      params: { slug: category.id }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps = async(context) => {
+  const { slug } = context.params;
+
+  const response = await fetch(`http://localhost:3333/products?category_id=${slug}`)
+  const products = await response.json();
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60,
+  }
+}
