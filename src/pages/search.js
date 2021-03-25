5b85ca9bc7e65cc3a94eecabf7032d4e9c4448e9
darianelourdes <darianelsantana@gmail.com>
2021-03-25 17:47:24 -0300

    search page

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,5 +1,64 @@
-export default function Search() {
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import Link from 'next/link';
+import { client } from '../lib/prismic';
+import Prismic from 'prismic-javascript';
+import PrismicDom from 'prismic-dom';
+
+
+export default function Search({ searchResults }) {
+  const router = useRouter();
+  const [search, setSearch] = useState('');
+
+  function handleSearch(e) {
+    e.preventDefault();
+
+    router.push(
+      `/search?q=${encodeURIComponent(search)}`
+    )
+
+    setSearch('');
+  }
+
   return (
-    <h1>Search</h1>
+    <div>
+      <form onSubmit={handleSearch}>
+        <input type="text" value={search} onChange={e => setSearch(e.target.value)} />
+        <button type="submit">Search</button>
+      </form>
+
+      <ul>
+        {searchResults.map(product => {
+          return (
+            <li key={product.id}>
+              <Link href={`/catalog/products/${product.uid}`}>
+                <a>
+                  {PrismicDom.RichText.asText(product.data.title)}
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   )
+}
+
+export const getServerSideProps = async (context) => {
+  const { q } = context.query;
+
+  if (!q) {
+    return { props: { searchResults: [] } };
+  }
+
+  const searchResults = await client().query([
+    Prismic.Predicates.at('document.type', 'product'),
+    Prismic.Predicates.fulltext('my.product.title', q)
+  ])
+
+  return {
+    props: {
+      searchResults: searchResults.results,
+    }
+  }
 }
